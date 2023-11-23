d5fc6eb0ac4ad608bb29adcb6d0b188730a9b7cd
ssm00 <titan0724@mju.ac.kr>
2023-11-24 01:25:02 +0900

    타이머, 게임매니저생성

---
+++ Assets/scr/sumin/GameManager.ts
@@ -0,0 +1,30 @@
+import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
+import Client from "./Client";
+import {GameObject} from "UnityEngine";
+
+//전체 시간, 백신 기계 위치 세팅, 백신 기계 수리 현황, 팀 나누기
+export default class GameManager extends ZepetoScriptBehaviour {
+
+    private client:Client;
+    public gamePlayTime:number;
+
+    //싱글톤
+    public static instance:GameManager = null
+    public static getInstance() {
+        if (this.instance == null) {
+            this.instance = GameObject.FindObjectOfType<GameManager>();
+        }
+        return this.instance
+    }
+
+    Start(){
+        this.client = Client.getInstance();
+    }
+
+    
+    //전체 게임 타이머 시작하기
+    private timerStart() {
+
+    }
+
+}
