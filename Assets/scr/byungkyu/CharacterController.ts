8bb2deb11e34dce6877afc12e250a0267de77133
Gyulguma <ybg653924@gmail.com>
2023-11-28 22:43:38 +0900

    button ui
    버튼 ui 추가

--- Assets/scr/byungkyu/CharacterController.ts
+++ Assets/scr/byungkyu/CharacterController.ts
@@ -1,39 +1,95 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script';
-import { Button } from 'UnityEngine.UI';
-import { Animator, AnimationClip } from 'UnityEngine';
-import { SpawnInfo, ZepetoPlayers, LocalPlayer, ZepetoCharacter } from 'ZEPETO.Character.Controller';
+import { Button, Image } from 'UnityEngine.UI';
+import { Animator, AnimationClip, HumanBodyBones, Vector3, WaitForEndOfFrame, Collider } from 'UnityEngine';
+import { SpawnInfo, ZepetoPlayers, LocalPlayer, ZepetoCharacter, ZepetoPlayer, KnowSockets } from 'ZEPETO.Character.Controller';
 import { WorldService } from 'ZEPETO.World';
 
 export default class CharacterController extends ZepetoScriptBehaviour {
 
-    public customAnimationClip: AnimationClip;
-    public playCustomAnimationButton: Button;
-    public playCustomAnimationButton2: Button;
-    public playCustomAnimationButton3: Button;
+    public crouchButton: Button;
+    public attackButton: Button;
+    public Button3: Button;
+    public isCrouch: bool;
+
     private _localPlayerAnimator: Animator;
+    private _zepetoCharacter: ZepetoCharacter;
+    private runSpeed: int;
+    private characterHeight: double;
+    private characterCenterY: double;
+    private characterVisualHeight: double;
+    private player: LocalPlayer
 
     Start() {
-
-        this.playCustomAnimationButton.onClick.AddListener(() => {
+        this.crouchButton.onClick.AddListener(() => {
+            this.isCrouch = !this.isCrouch;
             this._localPlayerAnimator.SetBool("isCrouch", !this._localPlayerAnimator.GetBool("isCrouch"));
+
+            if(this.isCrouch) this._zepetoCharacter.additionalRunSpeed = -this.runSpeed;
+            else this._zepetoCharacter.additionalRunSpeed = this.runSpeed;
+            this.StartCoroutine(this.CoSetZepetoPlayer());
         })
-        this.playCustomAnimationButton2.onClick.AddListener(() => {
-            this._localPlayerAnimator.SetTrigger("Attack");
-            this._localPlayerAnimator.CrossFade("Attack", 1, 1, 0.1);
+        this.attackButton.onClick.AddListener(() => {
+            if(this._localPlayerAnimator.GetBool("isZombie") == true){
+                this._localPlayerAnimator.SetTrigger("Attack");
+                this._localPlayerAnimator.CrossFade("Attack", 1, 1, 0.1);
+            }
+            
+            console.log("attack!");
+
         })
-        this.playCustomAnimationButton3.onClick.AddListener(() => {
-            this._localPlayerAnimator.SetTrigger("BeCatched");
-            this._localPlayerAnimator.SetBool("isZombie", true);
+        this.Button3.onClick.AddListener(() => {
+            if(this._localPlayerAnimator.GetBool("isZombie") == false){
+                this._localPlayerAnimator.SetTrigger("BeCatched");
+                this._localPlayerAnimator.SetBool("isZombie", true);
+            }
         })
 
         ZepetoPlayers.instance.CreatePlayerWithUserId(WorldService.userId, new SpawnInfo(), true);
 
         ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
-            const player: LocalPlayer = ZepetoPlayers.instance.LocalPlayer;
-            this._localPlayerAnimator = player.zepetoPlayer.character.GetComponentInChildren<Animator>();
+            this.player = ZepetoPlayers.instance.LocalPlayer;
+            this._localPlayerAnimator = this.player.zepetoPlayer.character.GetComponentInChildren<Animator>();
+            this._zepetoCharacter = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer.character;
+            this.runSpeed = 3;
+            this.StartCoroutine(this.CoGetZepetoHeight(this.player.zepetoPlayer));
+
+            this._zepetoCharacter.gameObject.tag = "Player";
+            console.log("middle: "+this._zepetoCharacter.tag);
         });
-        
+ 
     }
 
+    *CoGetZepetoHeight(zepeto: ZepetoPlayer) {
+        yield new WaitForEndOfFrame();
+        const headPosition = zepeto.character.GetSocket(KnowSockets.HEAD_UPPER).position;
+        const leftFootPosition = zepeto.character.ZepetoAnimator.GetBoneTransform(HumanBodyBones.LeftFoot).position;
+        const rightFootPosition = zepeto.character.ZepetoAnimator.GetBoneTransform(HumanBodyBones.RightFoot).position;
+        const characterCenter = Vector3.Lerp(leftFootPosition, rightFootPosition, 0.5);
+        this.characterHeight = Vector3.Distance(headPosition, characterCenter);
 
+        this.characterCenterY = this.player.zepetoPlayer.character.characterController.center.y;
+        this.characterVisualHeight = this.player.zepetoPlayer.character.characterController.height;
+   }
+
+   *CoSetZepetoPlayer(){
+        if(this.isCrouch){
+            let controllerHeight = this.characterHeight / 0.85 * 0.6;
+            let controllerCenterY = controllerHeight / 2;
+
+            this.player.zepetoPlayer.character.characterController.height = controllerHeight;
+            this.player.zepetoPlayer.character.characterController.center = new Vector3(0, controllerCenterY, 0);  
+        }
+        else{
+            this.player.zepetoPlayer.character.characterController.height = this.characterVisualHeight;
+            this.player.zepetoPlayer.character.characterController.center = new Vector3(0, this.characterCenterY, 0);  
+        }
+   }
+
+   OnCollisionEnter(collider: Collider){
+    console.log("collise");
+   }
+
+   OnTriggerEnter(collider: Collider){
+    console.log("collise");
+   }
 }
---
+++ Assets/scr/byungkyu/test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hi box");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        Debug.Log("box collision!!");
+    }
+}
