89e3c9813379f96afbcc5a669707f89ef8e34c23
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2023-11-28 22:51:25 +0900

    1
    1

--- Assets/scr/on/New Folder/InteractionIcon.ts
+++ Assets/scr/on/New Folder/InteractionIcon.ts
@@ -1,6 +1,6 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script';
-import { Camera, Canvas, Collider, GameObject, Transform, Object, AnimationClip} from 'UnityEngine';
-import { Button } from 'UnityEngine.UI';
+import { Camera, Canvas, Collider, GameObject, Transform, Object, AnimationClip, Animation, Time} from 'UnityEngine';
+import { Button, Text } from 'UnityEngine.UI';
 import { UnityEvent } from 'UnityEngine.Events';
 import { ZepetoPlayers } from 'ZEPETO.Character.Controller';
  
@@ -23,20 +23,41 @@ export default class InteractionIcon extends ZepetoScriptBehaviour {
     private _cachedWorldCamera: Camera;
     private _isIconActive: boolean = false;
     private _isDoneFirstTrig: boolean = false;
-     
-     
+    private animation : Animation; 
+    
+    // 백신 시간 체크
+    private startTime : float = 0;
+    private finishTime : float = 5;
+    private TimeCheck : boolean = false;
+
+    /* 백신 현황판
+    @Header("[Source]")
+    @SerializeField() private vaccine : Text;
+    private vaccineClear : int=5;
+    private vaccineCondition : int=0;*/
+
+    private Start() {
+        this.animation=this.gameObject.GetComponentInParent<Animation>();
+    }
     private Update() {
         if (this._isDoneFirstTrig && this._canvas?.gameObject.activeSelf) {
             this.UpdateIconRotation();
         }
+        if(this.TimeCheck) {
+            this.startTime+=Time.deltaTime;
+            if(this.startTime>this.finishTime) {
+                this.AnimationPlay();
+                //this.Vaccine();
+                this.TimeCheck=false;
+            }
+
+        }
     }
      
     private OnTriggerEnter(coll: Collider) {
-        console.log("A")
         if (coll != ZepetoPlayers.instance.LocalPlayer?.zepetoPlayer?.character.GetComponent<Collider>()) {
             return;
         }
-        console.log("B");
         this.ShowIcon();
         this.OnTriggerEnterEvent?.Invoke();
     }
@@ -47,13 +68,13 @@ export default class InteractionIcon extends ZepetoScriptBehaviour {
         }
          
         this.HideIcon();
+        this.TimeCheck=false;
+        this.startTime=0;
         this.OnTriggerExitEvent?.Invoke();
     }
      
     public ShowIcon(){
-        console.log("C");
         if (!this._isDoneFirstTrig) {
-            console.log("D");
             this.CreateIcon();
             this._isDoneFirstTrig = true;
         }
@@ -69,14 +90,11 @@ export default class InteractionIcon extends ZepetoScriptBehaviour {
     }
  
     private CreateIcon() {
-        console.log("E");
         if (this._canvas === undefined) {
-            console.log("F");
             const canvas = GameObject.Instantiate(this.prefIconCanvas, this.iconPosition) as GameObject;
             this._canvas = canvas.GetComponent<Canvas>();
             this._button = canvas.GetComponentInChildren<Button>();
             this._canvas.transform.position = this.iconPosition.position;
-            console.log("G");
         }
         this._cachedWorldCamera = Object.FindObjectOfType<Camera>();
         this._canvas.worldCamera = this._cachedWorldCamera;
@@ -92,6 +110,18 @@ export default class InteractionIcon extends ZepetoScriptBehaviour {
  
     private OnClickIcon() {
         this.OnClickEvent?.Invoke();
+        this.TimeCount();
+        
+    }
+    private AnimationPlay() {
+        this.animation.Play("Crate_Open");
     }    
+    private TimeCount() {
+        this.TimeCheck=true;
+    }
+    /*private Vaccine() {
+        this.vaccineCondition+=1;
+        this.vaccine.text = "백신 현황"+"("+this.vaccineCondition+"/"+this.vaccineClear+")";
+    }*/
     
 }
